bbad6c2b0fd65924427180df034aa0bfb18a4c1e
hashishaw <cshaw@hashicorp.com>
2022-03-09 16:04:24 -0600

    Fetch ID from self

--- ui/app/routes/interview.js
+++ ui/app/routes/interview.js
@@ -2,12 +2,14 @@ import Route from '@ember/routing/route';
 
 export default class InterviewRoute extends Route {
   model(params) {
-    console.log({ params });
-    // throw new Error('Whoops bad');
-    // return fetch(`/api/candidate/${params.id}`);
-    return {
-      name: 'Chelsea Shaw',
-      code: params.id,
-    };
+    return fetch(`/api/interview/${params.id}`, {
+      method: 'GET',
+    }).then((res) => {
+      console.log({ res });
+      return {
+        name: 'Chelsea Shaw',
+        code: params.id,
+      };
+    });
   }
 }
